2b2700d8879596f4eb6c0b8717f4362f0f7a005d
xfsong <1559251498@qq.com>
2022-05-14 22:27:14 +0800

    add configuration. It can be run in single way.

---
+++ Final-Proj/app/src/main/java/com/bytedance/sjtu/liuyi/Activity/IdeaActivity.kt
@@ -0,0 +1,135 @@
+package com.bytedance.sjtu.liuyi.Activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bytedance.sjtu.liuyi.Adapter.IdeaViewAdapter
+import com.bytedance.sjtu.liuyi.DataClass.IdeaItem
+import com.bytedance.sjtu.liuyi.IdeaItemDBHelper
+import com.bytedance.sjtu.liuyi.R
+import com.bytedance.sjtu.liuyi.databinding.ActivityScrollingBinding
+
+class IdeaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+    private lateinit var rvAdapter : IdeaViewAdapter
+    private lateinit var rvIdea: RecyclerView
+    private lateinit var dateStr : String
+    private val dbHelper = IdeaItemDBHelper(this, "idea.db", 1)
+    private var db : SQLiteDatabase? = null
+    private var databaseTestItem : Boolean = false
+    private var bundleTestItem : Boolean = false
+    private val ideaItemRequestLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        when (it.resultCode) {
+            IdeaItemCreationActivity.IdeaItemCreationSuccessCode -> {
+                rvAdapter.setIdeaList(dbHelper.getIdeaItemListByDate(dateStr))
+            }
+            IdeaItemCreationActivity.IdeaItemCreationFailCode -> {}
+        }
+    }
+
+    @SuppressLint("NewApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setBundleTestItem(true)    // if set true, default date bundle will be set for the database filter.
+        inflateTestDateBundle()
+        setEnvironmentParameter()
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        binding.toolbarLayout.title = title
+        binding.fab.setOnClickListener {
+            intent = Intent(this, IdeaItemCreationActivity::class.java)
+            ideaItemRequestLauncher.launch(intent)
+        }
+        setDatabaseTestItem(true)    // if set true, default items will be inserted into database for testing.
+        bindDatabase()
+        initRecyclerView()
+        clearDatabaseItem()
+    }
+
+    private fun getParameterBundle(): Bundle? {
+        return intent.extras
+    }
+
+    private fun setEnvironmentParameter() {
+        val bundle = getParameterBundle()
+        dateStr = "${bundle!!.get("year")}-${bundle.get("month")}-${bundle.get("day")}"
+    }
+
+    private fun inflateTestDateBundle() {
+        if (bundleTestItem) {
+            intent.putExtra("year", "2022")
+            intent.putExtra("month", "05")
+            intent.putExtra("day", "14")
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_scrolling, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun bindDatabase() {
+        db = dbHelper.writableDatabase
+        if (databaseTestItem) {
+            initTestIdeaItem()
+        }
+    }
+
+    private fun setBundleTestItem(b: Boolean) {
+        bundleTestItem = b
+    }
+
+    private fun setDatabaseTestItem(b: Boolean) {
+        databaseTestItem = b
+    }
+
+    private fun initTestIdeaItem() {
+        val ideaItemList = mutableListOf(
+            IdeaItem("2022-05-14", "Keep simple, keep stupid.", "", "", "2022-05-10-22-43-24"),
+            IdeaItem("2022-02-23", "Have a heart of spring, ecstatic to in full bloom.", "", "", "2022-02-23-15-42-52"),
+            )
+        for (ideaItem in ideaItemList) {
+            dbHelper.insertIdeaItem(db, ideaItem)
+        }
+    }
+
+    private fun clearDatabaseItem() {
+        dbHelper.deleteAllItem(db)
+    }
+
+    private fun initRecyclerView() {
+        val rv = findViewById<RecyclerView>(R.id.idea_recycler_view)
+        val layoutManager = LinearLayoutManager(this)
+        rv.layoutManager = layoutManager
+        val adapter = IdeaViewAdapter()
+        adapter.setIdeaList(dbHelper.getIdeaItemListByDate(dateStr))
+        rvAdapter = adapter
+        rv.adapter = adapter
+        rvIdea = rv
+    }
+
+
+
+}
